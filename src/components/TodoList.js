0db6f0f0480a6dc70f9f5776d486472ddd2f7a36
Jair Partida <jairpartida1@gmail.com>
2021-08-23 22:18:50 -0600

    functionality done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,11 +15,20 @@ function TodoList() {
         setTodos(newTodos);
     };
 
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    };
+
     const removeTodo = id => {
         const removeArr = [...todos].filter(todo => todo.id !== id);
 
         setTodos(removeArr);
     };
+
 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
@@ -35,9 +44,9 @@ function TodoList() {
         <div>
             <h1>What's the plan for today?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
     )
 }
 
-export default TodoList
+export default TodoList;
